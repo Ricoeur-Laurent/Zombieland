174b315d390618357e6d2dd8f882e01c1e6db24a
GabrielAmand <gabrielamand@yahoo.com>
2025-07-07 14:58:53 +0200

    feat/connexion done with some testing, view /components/connexions/LoginForm

--- front/src/app/connexion/page.tsx
+++ front/src/app/connexion/page.tsx
@@ -1,7 +1,16 @@
-// import { useState } from "react"
 
-// export default ConnexionPage (){
-//     const[email, setEmail] = useState("");
-//     const[password, setPassword] = useState("");
-//     const[error, setError] = useState("");
-// }
+import ConnexionForm from "@/components/connexion/LoginForm";
+
+export const metadata = {
+	title: "connexion ZombieLand",
+	description:
+		"page to connect to ZombieLand.",
+};
+
+export default function ConnexionPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <ConnexionForm />
+    </div>
+  );
+}
--- front/src/context/ TokenProvider.tsx
+++ front/src/context/ TokenProvider.tsx
@@ -2,7 +2,7 @@
 // When to use use client in NextJs?
 // The 'use client' directive declares an entry point for the components to be rendered on the client side and should be used when creating interactive user interfaces (UI) that require client-side JavaScript capabilities, such as state management, event handling, and access to browser APIs.
 
-import { type ReactNode, createContext, useContext, useState } from "react";
+import { type ReactNode, createContext, useContext, useEffect, useState } from "react";
 
 // Context type-safe
 // we create an empty context that 'll be filled when the api provide it
@@ -16,6 +16,23 @@ export const TokenContext = createContext<
 
 export default function TokenProvider({ children }: { children: ReactNode }) {
   const [token, setToken] = useState<string | null>(null);
+
+  // if there is a token , charge it in the localstorage
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  // update local storage as soon as the token change
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
 
   return (
     <TokenContext.Provider value={{ token, setToken }}>
