d5315ca3867b4408c8dc4a666c89aa61e8f66f6b
Julien_Desbard <julien.desbard@gmail.com>
2025-07-08 16:08:54 +0200

    test : force : true for sequelize

--- back/index.js
+++ back/index.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './src/routers/router.js';
-import { sequelize } from './src/models/sequelize.js';
 import seed from './src/migration/seed.js';
+import { initDatabase } from './src/migration/sync.js';
+
 
 dotenv.config();
 
@@ -15,12 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router);
 const PORT = process.env.PORT || 5000;
 
-// integration of SYnc in index to run it when launching API live on render
-sequelize.sync({ alter: true }) // ou force: true temporairement
-  .then(() => console.log('✅ Tables synchronisées'))
-  .catch(err => console.error('❌ Erreur sync Sequelize', err));
-
-seed()
+// init database for Render server
+initDatabase();
+seed();
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
