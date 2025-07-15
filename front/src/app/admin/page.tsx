0048d080c87ab493d1ca8c3d476c8431bcb94f6f
GabrielAmand <gabrielamand@yahoo.com>
2025-07-15 09:46:44 +0200

    fix metada and file name

---
+++ front/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+	title: "admin – Zombieland",
+	description: "back office de Zombieland.",
+};
+
+export default function AdminPage() {
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-start px-4 py-12 bg-background text-text">
+			<h1>hello admin</h1>
+		</div>
+	);
+}
