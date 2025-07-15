b0815b1ea18da2fe9e2e4f35d61d92cd12ab0417
Ricoeur-Laurent <ricoeurlaurent@gmail.com>
2025-07-15 10:32:55 +0200

    feat: redirect admin page

--- front/src/app/admin/page.tsx
+++ front/src/app/admin/page.tsx
@@ -1,9 +1,19 @@
-export const metadata = {
-	title: "admin – Zombieland",
-	description: "back office de Zombieland.",
-};
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useTokenContext } from "@/context/TokenProvider";
 
 export default function AdminPage() {
+	const { user } = useTokenContext();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user === null) {
+			router.replace("/connexion");
+		} else if (!user.admin) {
+			router.replace("/unauthorized");
+		}
+	}, [user, router]);
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-start px-4 py-12 bg-background text-text">
 			<h1>hello admin</h1>
