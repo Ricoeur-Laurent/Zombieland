d29b7a59a67feac64cc0222d33780b405eebfb7b
GabrielAmand <gabrielamand@yahoo.com>
2025-07-22 15:06:20 +0200

    removed wrong code for the fix

--- back/src/routers/routes/auth.routes.js
+++ back/src/routers/routes/auth.routes.js
@@ -3,17 +3,6 @@ import { verifyToken } from '../../middlewares/verifyToken.js';
 
 const router = express.Router();
 
-router.options('/verify', (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://zombieland-front-vercel.vercel.app');
-	res.setHeader('Access-Control-Allow-Credentials', 'true');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.setHeader('Vary', 'Origin');
-	return res.sendStatus(204);
-  });
-  
-
-// Route de vérification du token
 router.get('/verify', verifyToken, (req, res) => {
 
 
