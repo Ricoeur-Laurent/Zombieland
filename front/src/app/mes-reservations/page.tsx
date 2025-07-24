d97597da4591e0bf4823c17255d501be487a95b1
Ricoeur-Laurent <ricoeurlaurent@gmail.com>
2025-07-24 13:05:43 +0200

    fix/size map

--- front/src/app/mes-reservations/page.tsx
+++ front/src/app/mes-reservations/page.tsx
@@ -1,6 +1,5 @@
-import ReservationList from "@/components/mes-reservations/ReservationList";
 import { Suspense } from "react";
-
+import ReservationList from "@/components/mes-reservations/ReservationList";
 
 export const metadata = {
 	title: "Mes réservations – Zombieland",
@@ -11,10 +10,10 @@ export const metadata = {
 export default function MesReservationsPage() {
 	return (
 		<div className="max-w-md mx-auto p-4 px-4 py-6 ">
-				<h2 className="text-primary font-subtitle text-3xl text-center mt-4">
+			<h2 className="text-primary font-subtitle text-3xl text-center mt-4">
 				Mes Réservations
 			</h2>
-			<Suspense  fallback={<p className="text-center mt-6">Chargement...</p>}>
+			<Suspense fallback={<p className="text-center mt-6">Chargement...</p>}>
 				<ReservationList />
 			</Suspense>
 		</div>
