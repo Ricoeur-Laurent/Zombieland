3b92f72093df0420e326d8b3eb96ebc64c214a1b
Ricoeur-Laurent <ricoeurlaurent@gmail.com>
2025-07-24 15:20:33 +0200

    fix bug

--- front/src/app/reservations/page.tsx
+++ front/src/app/reservations/page.tsx
@@ -1,6 +1,5 @@
-import ReservationForm from "@/components/reservations/ReservationForm";
 import { Suspense } from "react";
-
+import ReservationForm from "@/components/reservations/ReservationForm";
 
 export const metadata = {
 	title: "Réservations – Zombieland",
@@ -9,15 +8,13 @@ export const metadata = {
 };
 export default function ReservationsPage() {
 	return (
-
-<div className="px-4 sm:px-6 md:px-8 py-10 max-w-6xl mx-auto text-text
-		<h2 className="text-center text-3xl sm:text-5xl font-subtitle uppercase text-primary mb-8">
-			Réserver votre visite
-		</h2>
-			<Suspense  fallback={<p className="text-center mt-6">Chargement...</p>}>
+		<div className="px-4 sm:px-6 md:px-8 py-10 max-w-6xl mx-auto text-text">
+			<h2 className="text-center text-3xl sm:text-5xl font-subtitle uppercase text-primary mb-8">
+				Réserver votre visite
+			</h2>
+			<Suspense fallback={<p className="text-center mt-6">Chargement...</p>}>
 				<ReservationForm />
 			</Suspense>
-
 		</div>
 	);
 }
